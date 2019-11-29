0acc2b4224e3a05f421ddc867e8a4009ec5651ce
ysy <16302010076@fudan.edu.cn>
2019-11-29 17:36:40 +0800

    write predicated rating into csv file

--- test.py
+++ test.py
@@ -2,12 +2,15 @@ import user_based as ub
 import item_based as ib
 import util
 import numpy
+import pandas
 
-target_matrix = util.read_file("test.csv")
+# import DataFrame
+
+# target_matrix = util.read_file("test.csv")
 print('\n', "Test User Based Recommendation System", '\n')
 UB = ub.userBasedRecommSys()
-# UB.setup("train_mini_test.csv", 3)
-UB.setup("train.csv", 100)
+UB.setup("train_mini_test.csv", 3)
+# UB.setup("train.csv", 100)
 print("preference matrix:")
 print(UB.preference_matrix, '\n')
 print("user similarity matrix:")
@@ -16,11 +19,13 @@ print("user average rate array:")
 print(UB.average_rate_array, '\n')
 print("user k nearest neighbors:")
 print(UB.k_nearest_neighbor, '\n')
-UB.predicate_rating()
 print("predicate rating based on user")
+UB.predicate_rating()
 print(UB.preference_matrix, '\n')
+util.save_csv(UB.preference_matrix, "user_based_predicate.csv")
+
 print("RMSE:")
-print(util.RMSE(numpy.array(UB.preference_matrix), numpy.array(target_matrix)))
+# print(util.RMSE(numpy.array(UB.preference_matrix), numpy.array(target_matrix)))
 
 
 print('\n', "Test Item Based Recommendation System", '\n')
@@ -28,8 +33,9 @@ IB = ib.itemBasedRecommSys()
 IB.setup("train.csv")
 print("item similarity matrix:")
 print(IB.item_similarity_matrix, '\n')
-IB.predicate_rating()
 print("predicate rating based on item")
+# IB.predicate_rating()
 print(IB.preference_matrix)
+util.save_csv(IB.preference_matrix, "item_based_predicate.csv")
 print("RMSE:")
-print(util.RMSE(numpy.array(IB.preference_matrix), numpy.array(target_matrix)))
+# print(util.RMSE(numpy.array(IB.preference_matrix), numpy.array(target_matrix)))
--- util.py
+++ util.py
@@ -1,3 +1,4 @@
+import pandas
 import pandas as pd
 import numpy
 
@@ -20,6 +21,17 @@ def read_file(path):
 preference matrix写入csv文件
 '''
 
+
+def save_csv(matrix, path):
+    data = pandas.DataFrame(matrix)
+    data = data.stack()
+    data.index = data.index.rename('userID', level=0)
+    data.index = data.index.rename('itemID', level=1)
+    data.name = 'rating'
+    data = data.reset_index()
+    numpy.savetxt(path, data, delimiter=',', header='userID,itemID,rating', comments='', newline='\n',fmt='%0i,%0i,%10.7f')
+
+
 '''
 计算估计值与实际值的RMSE 差
 rmse(numpy.array(),numpy.array())
@@ -37,3 +49,11 @@ def RMSE(predictions, targets):
                与预测的csv文件比较
 方法二比较可取 √
 '''
+import pandas as pd
+
+a = ['one', 'two', 'three']
+b = [1, 2, 3]
+english_column = pd.Series(a, name='english')
+number_column = pd.Series(b, name='number')
+predictions = pd.concat([english_column, number_column], axis=1)
+print(predictions)
