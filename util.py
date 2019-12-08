a12b4cb3b1cd4a5a64bce1f0ef760bfe852078af
Yanshiyi <shiyi.yan@sap.com>
2019-12-08 20:41:19 +0800

    user based & item based 完成啦!

--- util.py
+++ util.py
@@ -48,35 +48,6 @@ def save_csv_from_rating(matrix, path):
 
 
 '''
-计算两个向量的Pearson 相关性
-取值区间为[-1,1]。-1表示完全的负相关，+1表示完全的正相关，0表示没有线性相关。
-皮尔森相关系数描述的是线性关系。严格来说，需要数据集是正态分布的，但不必是零均值的。
-皮尔森相关系数的计算是先对向量每一分量减去分量均值，再求余弦相似度。这一操作称为中心化(将特征向量X根据 x¯\bar x¯x 移动)。
-Pearson 相关性：两个变量的协方差除于两个变量的标准差
-'''
-
-
-def corrcoef(preference_matrix, u, v):
-    # 求和
-    x = preference_matrix[u]
-    y = preference_matrix[v]
-    sum1 = sum(x)
-    sum2 = sum(y)
-    # 求乘积之和
-    sum1Sq = sum([pow(v, 2) for v in x])
-    sum2Sq = sum([pow(v, 2) for v in y])
-
-    pSum = sum([x[i] * y[i] for i in range(len(x))])
-
-    num = pSum - (sum1 * sum2 / len(x))
-    den = numpy.sqrt((sum1Sq - pow(sum1, 2) / len(x)) * (sum2Sq - pow(sum2, 2) / len(y)))
-
-    if den == 0.0:
-        return 0.0
-    return num / den
-
-
-'''
 计算估计值与实际值的RMSE 差
 rmse(numpy.array(),numpy.array())
 '''
